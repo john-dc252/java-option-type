b616dc2cb323cf3c4e099032bc6b691c43910bd3
john-dc252 <46738260+john-dc252@users.noreply.github.com>
2025-05-18 01:13:58 +0800

    test: switch to `testng`, refactor and add tests

--- src/test/java/ph/com/jndev/OptionTest.java
+++ src/test/java/ph/com/jndev/OptionTest.java
@@ -1,11 +1,12 @@
 package ph.com.jndev;
 
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThrows;
+import static org.testng.Assert.expectThrows;
 import static ph.com.jndev.Option.none;
 import static ph.com.jndev.Option.some;
 
@@ -14,26 +15,59 @@ public class OptionTest {
     public void testNoneValue() {
         final var noneValue = none();
 
-        assertThat(noneValue.isSome(), is(false));
-        assertThat(noneValue.isNone(), is(true));
-
-        assertThrows(NullPointerException.class, noneValue::unwrap);
+        expectNone(noneValue);
     }
 
     @Test
-    public void testOptionType() {
+    public void testSomeValue() {
         final var intValue = some(1);
 
-        assertThat(intValue.isSome(), is(true));
-        assertThat(intValue.isNone(), is(false));
-        assertThat(intValue.unwrap(), is(1));
+        expectSome(intValue, 1);
     }
 
     @Test
-    public void testOptionMap() {
+    public void testSomeMap() {
         final var intValue = some(1);
         final var stringValue = intValue.map(String::valueOf);
 
-        assertThat(stringValue.unwrap(), is("1"));
+        expectSome(stringValue, "1");
+    }
+
+    @Test
+    public void testNoneMap() {
+        final var noneValue = none();
+        final var stringValue = noneValue.map(String::valueOf);
+
+        expectNone(stringValue);
+    }
+
+    @Test
+    public void testSomeStream() {
+        final var someStream = some(1).stream();
+        assertThat(someStream.toList(), contains(1));
+    }
+
+    @Test
+    public void testNoneStream() {
+        final var noneStream = none().stream();
+        assertThat(noneStream.toList(), empty());
+    }
+
+    @Test(description = "`Some` should map to `None` if `mapFn` returns `null`")
+    public void testSomeMapNull() {
+        final var mappedValue = some(1).map(n -> null);
+        expectNone(mappedValue);
+    }
+
+    private static <T> void expectSome(Option<T> optionValue, T unwrappedValue) {
+        assertThat(optionValue.isSome(), is(true));
+        assertThat(optionValue.isNone(), is(false));
+        assertThat(optionValue.unwrap(), is(unwrappedValue));
+    }
+
+    private static <T> void expectNone(Option<T> noneValue) {
+        assertThat(noneValue.isSome(), is(false));
+        assertThat(noneValue.isNone(), is(true));
+        expectThrows(NullPointerException.class, noneValue::unwrap);
     }
 }
