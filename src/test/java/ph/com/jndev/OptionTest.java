f57554913bf87383803a39865a431aae69b19337
john-dc252 <46738260+john-dc252@users.noreply.github.com>
2025-05-20 23:39:18 +0800

    test: improve coverage (#5)

--- src/test/java/ph/com/jndev/OptionTest.java
+++ src/test/java/ph/com/jndev/OptionTest.java
@@ -2,9 +2,11 @@ package ph.com.jndev;
 
 import org.testng.annotations.Test;
 
+import java.util.Optional;
+import java.util.function.Predicate;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 import static org.testng.Assert.expectThrows;
 import static ph.com.jndev.Option.none;
@@ -26,6 +28,66 @@ public class OptionTest {
     }
 
     @Test
+    public void testSomeUnwrapOr() {
+        final var primitiveValue = 1;
+        final var intValue = some(primitiveValue);
+        final var result = intValue.unwrapOr(2);
+
+        assertThat(result, is(primitiveValue));
+    }
+
+    @Test
+    public void testNoneUnwrapOr() {
+        final var intValue = none();
+        final var defaultValue = 2;
+        final var result = intValue.unwrapOr(defaultValue);
+
+        assertThat(result, is(defaultValue));
+    }
+
+    @Test
+    public void testSomeUnwrapOrFn() {
+        final var primitiveValue = 1;
+        final var intValue = some(primitiveValue);
+        final var result = intValue.unwrapOr(() -> 2);
+
+        assertThat(result, is(primitiveValue));
+    }
+
+    @Test
+    public void testNoneUnwrapOrFn() {
+        final var intValue = none();
+        final var defaultValue = 2;
+        final var result = intValue.unwrapOr(() -> defaultValue);
+
+        assertThat(result, is(defaultValue));
+    }
+
+    @Test(description = "calling filter on a `Some` with a value that matches the predicate should return the same `Some`")
+    public void testSomeFilterSome() {
+        final var someValue = some(1);
+        final var result = someValue.filter(Predicate.isEqual(1));
+
+        assertThat(result, sameInstance(someValue));
+    }
+
+    @Test(description = "calling filter on a `Some` with a value that DOES NOT match the predicate should return `None`")
+    public void testSomeFilterNone() {
+        final var someValue = some(1);
+        final var result = someValue.filter(Predicate.isEqual(2));
+
+        assertThat(result, is(none()));
+    }
+
+    @Test(description = "calling filter on a `None` always returns `None`")
+    public void testNoneFilterNone() {
+        final var noneValue = none();
+        final var result = noneValue.filter(Predicate.isEqual(1));
+
+        assertThat(result, is(none()));
+    }
+
+    @Test
     public void testSomeMap() {
         final var intValue = some(1);
         final var stringValue = intValue.map(String::valueOf);
@@ -39,6 +101,48 @@ public class OptionTest {
         final var stringValue = noneValue.map(String::valueOf);
 
         expectNone(stringValue);
+    }
+
+    @Test
+    public void testSomeFlatMap() {
+        final var intValue = some(1);
+        final var stringValue = intValue.flatMap(v -> some(v.toString()));
+
+        expectSome(stringValue, "1");
+    }
+
+    @Test
+    public void testNoneFlatMap() {
+        final var noneValue = none();
+        final var stringValue = noneValue.flatMap(v -> some(v.toString()));
+
+        expectNone(stringValue);
+    }
+
+    @Test
+    public void testSomeFromOptional() {
+        final var primitiveValue = 1;
+        final var optional = Optional.of(primitiveValue);
+        final var someValue = Option.fromOptional(optional);
+
+        expectSome(someValue, primitiveValue);
+    }
+
+    @Test
+    public void testNoneFromOptional() {
+        final var optional = Optional.empty();
+        final var someValue = Option.fromOptional(optional);
+
+        expectNone(someValue);
+    }
+
+    @Test
+    public void testToString() {
+        final var someValue = some(1);
+        final var noneValue = none();
+
+        assertThat(someValue.toString(), is("Some(1)"));
+        assertThat(noneValue.toString(), is("None"));
     }
 
     @Test
